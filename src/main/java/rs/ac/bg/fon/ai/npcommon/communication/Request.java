ae9a22b64839053364d7b83932e38ea8196f9883
AleksandraMisic762 <am20170003@student.fon.bg.ac.rs>
2021-06-07 17:10:06 +0200

    Klase za komunikaciju Request i Response adaptirane za JSON format

--- src/main/java/rs/ac/bg/fon/ai/npcommon/communication/Request.java
+++ src/main/java/rs/ac/bg/fon/ai/npcommon/communication/Request.java
@@ -2,35 +2,29 @@ package  rs.ac.bg.fon.ai.npcommon.communication;
 
 import java.io.Serializable;
 
+import com.google.gson.Gson;
+
 public class Request implements Serializable{
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Operation operation;
-    private Object argument;
+//	private Operation operation;
+//    private Object argument;
+	private Gson argument;
 
     public Request() {
     }
 
-    public Request(Operation operation, Object argument) {
-        this.operation = operation;
+    public Request(Gson argument) {
         this.argument = argument;
     }
 
-    public Operation getOperation() {
-        return operation;
-    }
-
-    public void setOperation(Operation operation) {
-        this.operation = operation;
-    }
-
-    public Object getArgument() {
+    public Gson getArgument() {
         return argument;
     }
 
-    public void setArgument(Object argument) {
+    public void setArgument(Gson argument) {
         this.argument = argument;
     }
     
--- src/main/java/rs/ac/bg/fon/ai/npcommon/communication/Response.java
+++ src/main/java/rs/ac/bg/fon/ai/npcommon/communication/Response.java
@@ -2,37 +2,31 @@ package  rs.ac.bg.fon.ai.npcommon.communication;
 
 import java.io.Serializable;
 
+import com.google.gson.Gson;
+
 public class Response  implements Serializable{
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private Object result;
-    private Exception exception;
+//	private Object result;
+//    private Exception exception;
+	private Gson result;
 
     public Response() {
     }
 
-    public Response(Object result, Exception exception) {
+    public Response(Gson result) {
         this.result = result;
-        this.exception = exception;
     }
 
-    public Object getResult() {
+    public Gson getResult() {
         return result;
     }
 
-    public void setResult(Object result) {
+    public void setResult(Gson result) {
         this.result = result;
     }
-
-    public Exception getException() {
-        return exception;
-    }
-
-    public void setException(Exception exception) {
-        this.exception = exception;
-    }
-    
+  
     
 }
